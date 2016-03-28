810dbb90f1579d1a475bcd68294d2bc8b5547274
Johnny <johnnymustang7@gmail.com>
2016-03-28 16:31:21 -0500

    Finished design of options page and customizations page xml, also added options page java file.

---
+++ app/src/main/java/com/test/operationpenguinpanic/OptionsPage.java
@@ -0,0 +1,31 @@
+package com.test.operationpenguinpanic;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+/**
+ * Created by johnny on 3/28/16.
+ */
+public class OptionsPage {
+
+/*    final CheckBox soundY = (CheckBox) findViewById(R.id.soundY);
+    final CheckBox soundN = (CheckBox) findViewById(R.id.soundN);
+    final CheckBox buttonY = (CheckBox) findViewById(R.id.ButtonY);
+    final CheckBox buttonN = (CheckBox) findViewById(R.id.ButtonN);
+    final CheckBox musicY = (CheckBox) findViewById(R.id.MusicY);
+    final CheckBox musicN = (CheckBox) findViewById(R.id.MusicN);
+    */
+    public void checkChanged(){
+
+    }
+
+    public void musicCheckedNo(){
+
+    }
+
+}
